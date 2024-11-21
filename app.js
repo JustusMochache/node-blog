6e7d5ff2e09c8100de6bfa83821d6799a0b0087f
Justus Mochache Onyancha <justus.onyancha@griffinglobaltech.com>
2024-11-21 18:08:04 +0300

    add main server and routes

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+//after creating the dotenv now we can use it in our application
 require('dotenv').config();
+
+// create a very basic express server
+
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+
+app.use(express.static('public'));
+
+//templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'))
+
+
+app.listen(PORT, ()=> {
+    console.log(`App listening on port ${PORT} `);
+    
+});
+
